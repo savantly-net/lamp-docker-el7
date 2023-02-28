7d42fc757b7f4cfbbeb4f72b971daf8e0e7c46a6
Jeremy <jeremy@savantly.net>
2023-02-27 19:54:17 -0600

    update deploy for multi-arch

--- deploy.sh
+++ deploy.sh
@@ -5,5 +5,9 @@ REPO_URL=savantly/lamp-docker:8.0
 DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && pwd )"
 cd $DIR
 
-docker build -t $REPO_URL .
+docker buildx build \
+--push \
+--platform linux/arm/v7,linux/arm64/v8,linux/amd64 \
+-t $REPO_URL .
+
 docker push $REPO_URL
