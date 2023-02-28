58dd649448e42312def6389565abac26b17da951
Jeremy <jeremy@savantly.net>
2023-02-27 20:03:59 -0600

    update build arch

--- deploy.sh
+++ deploy.sh
@@ -1,13 +1,13 @@
 #!/bin/bash
 
-REPO_URL=savantly/lamp-docker:8.0
+REPO_URL=savantly/lamp-docker:8.1
 
 DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && pwd )"
 cd $DIR
 
 docker buildx build \
 --push \
---platform linux/arm/v7,linux/arm64/v8,linux/amd64 \
+--platform linux/arm64/v8,linux/amd64 \
 -t $REPO_URL .
 
-docker push $REPO_URL
+#docker push $REPO_URL
